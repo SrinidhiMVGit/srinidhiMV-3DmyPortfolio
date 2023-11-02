04216a7aa928f128995f7e961ddb2c45ae4c9f1c
SrinidhiMVGit <SrinidhiMVGit123@gmail.com>
2023-11-02 20:53:13 +0530

    Minor fix

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -32,7 +32,7 @@ const Ball = (props) => {
 const BallCanvas = ({icon}) => {
   return (
     <Canvas
-      frameLoop="demand"
+      frameloop="demand"
       gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
