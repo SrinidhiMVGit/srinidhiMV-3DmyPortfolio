32dc82175ed636cca1df9e6ccb09d8b1b4215968
SrinidhiMVGit <SrinidhiMVGit123@gmail.com>
2023-11-02 21:39:44 +0530

    minor fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -27,7 +27,7 @@ const Navbar = () => {
 
   return (
     <nav
-      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 ${scrolled ? "bg-primary" : "bg-transparent"}`}
+      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20`}
     >
       <div className="w-full flex justify-between item-center max-w-7xl mx-auto">
         <Link
